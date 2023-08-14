04a42f9b31d2b587f02727815a18873f40de590d
Pablo UPB <pcvm2453@gmail.com>
2023-08-14 16:59:33 -0400

    Ejemplo some

---
+++ server/07_some.js
@@ -0,0 +1,22 @@
+//verificamos si almenos un numero es par
+/*const numbers = [1,11,3,5,7,9];
+
+let rsp = numbers.some(
+    (number) =>{
+        return (number%2===0)&&true;
+    }
+);
+console.log( numbers, rsp );*/
+
+//Uso de some con un objeto donde verificamos si un coche fue entregado a su dueño en un taller
+const automovil = [
+    {marca:'toyota',modelo:'caldina',anio:1999,entregado:true},
+    {marca:'nissan',modelo:'sunny',anio:1999,entregado:false},
+    {marca:'toyota',modelo:'corolla',anio:2000,entregado:false},
+    {marca:'nissan',modelo:'sentra',anio:2002,entregado:true},
+    {marca:'toyota',modelo:'spacio',anio:1999,entregado:true},
+    {marca:'suzuki',modelo:'vitara',anio:2020,entregado:true},
+];
+const reparadado = automovil
+.some( (auto) => auto.entregado );
+console.log( 'Autos reparados:'+reparadado );
