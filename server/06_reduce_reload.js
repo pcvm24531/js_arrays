0dfb158215afc3e8278ac1e1acacff6ff8a4a211
Pablo UPB <pcvm2453@gmail.com>
2023-08-14 15:51:46 -0400

    Se agregó ejemplo de reduce con objetos

--- server/06_reduce_reload.js
+++ server/06_reduce_reload.js
@@ -1,5 +1,5 @@
-const items = [1,2,3,4,3];
-
+//Ejemplo de reduce con arreglo
+/*const items = [1,2,3,4,3];
 const rsp = items.reduce(
     ( obj, item ) => {
         if( !obj[item] ){
@@ -8,7 +8,33 @@ const rsp = items.reduce(
             obj[item] = obj[item] + 1;
         }
         return obj;
-    }, {}
+    }, 
+    {}
+);
+console.log( items, rsp );*/
+
+//Ejemplo de reduce utilizando objetos
+const automovil = [
+    {marca:'toyota',modelo:'caldina',anio:1999},
+    {marca:'nissan',modelo:'sunny',anio:1999},
+    {marca:'toyota',modelo:'corolla',anio:2000},
+    {marca:'nissan',modelo:'sentra',anio:2002},
+    {marca:'toyota',modelo:'spacio',anio:1999},
+    {marca:'suzuki',modelo:'vitara',anio:2020},
+];
+//Aplicar reduce por marca
+const rsp = automovil
+.map( car => car.marca )
+.reduce(
+    (obj, mark) => {
+        if( !obj[mark] ){
+            obj[mark] = 1;
+        }else{
+            obj[mark] = obj[mark] + 1;
+        }
+        return obj;
+    },
+    {}
 );
 
-console.log( items, rsp );
+console.log( automovil, rsp );
