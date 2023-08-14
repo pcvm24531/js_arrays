0124a505fdaf07d81b099ff8b8398f1e369a04d3
Pablo UPB <pcvm2453@gmail.com>
2023-08-14 14:55:14 -0400

    Se agrego el ejemplo reduce_reload

--- server/06_reduce_reload.js
+++ server/06_reduce_reload.js
@@ -1,1 +1,14 @@
 const items = [1,2,3,4,3];
+
+const rsp = items.reduce(
+    ( obj, item ) => {
+        if( !obj[item] ){
+            obj[item] = 1;
+        }else{
+            obj[item] = obj[item] + 1;
+        }
+        return obj;
+    }, {}
+);
+
+console.log( items, rsp );
