ef4cb3cf201cac30e77f0d5ac06d5f4cefb48153
Pablo UPB <pcvm2453@gmail.com>
2023-08-17 16:51:27 -0400

    Se agregó ejemplos en concat y el archivo flat

--- server/14_concat.js
+++ server/14_concat.js
@@ -1,5 +1,5 @@
 /**
- * concat() => es un metodo que sirve para funcionar arrays, el parámetro que recibe es el array que queremos concatenar
+ * concat() => es un metodo que sirve para funcionar arrays, el parámetro que recibe es el array que queremos concatenar. Concat no modifica el array original, lo que hace es crear un nuevo array
  */
 
 //Ej: Funcionar 2 arreglos 
@@ -8,4 +8,20 @@ const fruits = ['apple', 'banana','orange'];
 const meal = food.concat(fruits);
 console.log(`food: ${food}`);
 console.log(`fruits: ${fruits}`);
-console.log(`meal: ${meal}`);
+console.log(`meal: ${meal}, meal type is:${typeof meal}`);
+
+//Ej:COncatenar 2 arreglos usando split operation, split operation solo funciona con arrays
+const rsp = [...food, ...fruits];
+console.log( ` ${rsp}, rsp type is: ${typeof rsp} ` );
+
+//Ej: Agregar un elemento al array
+const rsp1 = [...food, 'Hola'];
+console.log( rsp1 );
+
+//Ej: Error al agregar un elemento
+const rsp2 = [...food, ...'Hola'];
+console.log( rsp2 ); 
+
+//Ej: Modificar el arreglo original, agregando nuevos elementos
+food.push(fruits);//food.push(...fruits);
+console.log( food );
---
+++ server/15_flat.js
@@ -0,0 +1,3 @@
+/**
+ * flat() => es inmutable, retorna un arreglo donde los subarrays han sido aplanados hasta una profundidad especifica.
+ */
